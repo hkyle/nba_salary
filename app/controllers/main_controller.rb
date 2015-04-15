c1a50092564a7731d6a2a158fb6cb3566f3c3f6d
Kyle Holgate <kyle.holgate@gmail.com>
2015-04-15 17:51:14 -0400

    refactored scraper

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,34 +1,11 @@
 class MainController < ApplicationController
   def index
-    #Scraper.scrape_salaries
+    Scraper.scrape_salaries
   end
   
   def charts
     @contracts = Contract.year(params[:year])
     @team_data = Team.all.order(:abbr)
-=begin
-    @chart = LazyHighCharts::HighChart.new('graph') do |f|
-      f.title(:text => "Salary By Team")
-      f.xAxis(:categories => @team_data.map{|i| i.abbr})
-      f.series(:name => "Salary", :yAxis => 0, :data => @team_data.map{|i| i.contracts.year(params[:year]).sum(:salary).floor }, :drilldown => 'subdept')
-      f.yAxis [
-        {:title => {:text => "Salary $"} }
-      ]
-    
-      f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical',)
-      f.chart({:defaultSeriesType=>"column"})
-      
-      f.drilldown(series: {
-        name:"Dept. Score",
-        id: 'subdept',
-        data: [
-        ["One", 1000],
-        ["Two", 20000000],
-        ["Three", 300000]
-      ]
-        })
-
-    end
-=end
+  
   end
 end
--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -1,32 +1,40 @@
 class Scraper
   require 'open-uri'
   
+  def self.get_disp_html(html_in)
+    # takes one line of HTML like "<th data-stat="y1" align="CENTER"  class="tooltip" >2014-15</th>"
+    # and returns what is between brackets, "2014-15"
+    html_in.to_s.gsub(/(\<.*?\>)/,'')
+  end
+  
   def self.scrape_salaries()
     url = 'http://www.basketball-reference.com/contracts/players.html'
-    stat_lines = []
 
       doc = Nokogiri::HTML(open(url))
-      vals = {}
       stats = doc.xpath('//table[contains(@id, "contracts")]//tbody//tr')
       
+      #this will get the year header, e.g. "2014-15", and split that out to only the first year (2014)
+      first_year = get_disp_html(doc.xpath('//table[contains(@id, "contracts")]//tbody//tr//th[contains(@data-stat, "y1")]').first).split('-')[0].to_i
 
       stats.each_with_index do |row, index|
           #only interested in the salaries for next 4 years
           if row['class'].blank?
-            cols = row.search('td')[1,7].map{ |x| x.to_s.gsub(/(\<.*?\>)/,'')} #stripping everything in brackets!
+            cols = row.search('td')[1,7].map{ |x| get_disp_html(x)} #stripping everything in brackets!
 
              if cols[0].nil? == false #make sure we have a player name
                p = Player.find_or_create_by(name: cols[0].to_s)
                t = Team.find_or_create_by(abbr: cols[1].to_s)
                
-               for i in 2..5
+               #check if each element (year) has salary given
+               for i in 2..5 
                  if !cols[i].blank?
-                   c = Contract.find_or_create_by(year: (2012+i).to_s+'-'+(2013+i).to_s, salary: cols[i].tr!('$,',''), player_id: p.id, team_id: t.id)
+                   c = Contract.find_or_create_by(year: (first_year - 2 + i).to_s+'-'+(first_year - 1 + i).to_s, salary: cols[i].tr!('$,',''), player_id: p.id, team_id: t.id)
                  end
                end
              end
-           end
+             
           end
-    end
+      end
+  end
     
 end
