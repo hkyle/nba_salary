78645c54e13b17e9b8ef77ce56b2e731c9f1a6e5
Kyle Holgate <kyle.holgate@gmail.com>
2015-04-29 18:32:27 -0400

    added all stats

--- app/models/stat_chart.rb
+++ app/models/stat_chart.rb
@@ -6,8 +6,13 @@ class StatChart < Chart
     @stat = params[:stat].downcase
   end
   
-  def stat
-    @stat
+  def self.stat_list
+    list = [ {val: 'per', text: 'PER'}, {val: 'ts_pct', text: 'True Shooting %'}, {val: 'trb_pct', text: 'Rebounding %'}, {val: 'ast_pct', text: 'Assist %'}, 
+             {val: 'stl_pct', text: 'Steal %'}, {val: 'blk_pct', text: 'Block %'}, {val: 'tov_pct', text: 'Turnover %'}, {val: 'usg_pct', text: 'Usage %'}, 
+             {val: 'ows', text: 'Offensive Win Shares'}, {val: 'dws', text: 'Defensive Win Shares'}, {val: 'ws', text: 'Win Shares'}, {val: 'ws_48', text: 'Win Shares/48 Mins'} 
+           ]
+           
+    list.sort_by{ |l| l[:text]}
   end
   
   def teams
