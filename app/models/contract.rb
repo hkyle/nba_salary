501da58e86fd2b1f296a7c1ca2a8561042b95027
Kyle Holgate <kyle.holgate@gmail.com>
2015-07-09 16:18:21 -0400

    added salary cap levels

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -3,4 +3,13 @@ class Contract < ActiveRecord::Base
   belongs_to :team
   scope :year, lambda{|y| where("year IN (?)", y )}
   
+  def self.salary_cap(year)
+    case year
+    when '2014-2015'
+      [63065000, 76829000]
+    when '2015-2016'
+      [70000000, 84740000]
+    end
+  end
+  
 end
